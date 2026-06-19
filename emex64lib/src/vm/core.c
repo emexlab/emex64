1ed2836db670963a009830d8240cb1efcf95f5f8
mach-port-t <fridakitten@pm.me>
2026-06-19 06:42:19 +0200

    fix: revert the not incrementation of the PC register (shall not be necessary)

--- emex64lib/src/vm/core.c
+++ emex64lib/src/vm/core.c
@@ -227,11 +227,7 @@ escape_from_la:
 
     /* the part of executing the instruction */
     core->op.op.func(core);
-
-    if(core->cr_state.crexc.exception == kEmex64ExceptionNone)
-    {
-        core->rl[kEmex64RegisterPC] += core->op.ilen;   /* FIXME: IDK if it should increment or not due to interrupts */
-    }
+    core->rl[kEmex64RegisterPC] += core->op.ilen;   /* FIXME: IDK if it should increment or not due to interrupts */
 
     return;
 }
