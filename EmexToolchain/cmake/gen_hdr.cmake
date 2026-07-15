8a1c3707ed1b9fdfd82081d6b95be7ba6e01bf5b
mach-port-t <fridakitten@pm.me>
2026-07-15 15:54:13 +0000

    fix: correctly tagging commit offsets in versioning

--- EmexToolchain/cmake/gen_hdr.cmake
+++ EmexToolchain/cmake/gen_hdr.cmake
@@ -2,29 +2,55 @@ find_package(Git QUIET)
 
 if(GIT_FOUND)
     execute_process(
-        COMMAND ${GIT_EXECUTABLE} rev-list --count HEAD
-        OUTPUT_VARIABLE EMEX64_BUILD_NUMBER
-        OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET)
-    execute_process(
         COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD
         OUTPUT_VARIABLE EMEX64_GIT_HASH
-        OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET)
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        ERROR_QUIET)
     execute_process(
-        COMMAND ${GIT_EXECUTABLE} describe --tags --abbrev=0
+        COMMAND ${GIT_EXECUTABLE} describe --tags --exact-match
         OUTPUT_VARIABLE EMEX64_VERSION_TAG
-        OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET)
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        RESULT_VARIABLE EMEX64_TAG_RESULT
+        ERROR_QUIET)
+    if(EMEX64_TAG_RESULT EQUAL 0)
+        set(EMEX64_COMMITS_SINCE_TAG 0)
+    else()
+        execute_process(
+            COMMAND ${GIT_EXECUTABLE} describe --tags --abbrev=0
+            OUTPUT_VARIABLE EMEX64_VERSION_TAG
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            ERROR_QUIET)
+        if(EMEX64_VERSION_TAG)
+            execute_process(
+                COMMAND ${GIT_EXECUTABLE} rev-list --count ${EMEX64_VERSION_TAG}..HEAD
+                OUTPUT_VARIABLE EMEX64_COMMITS_SINCE_TAG
+                OUTPUT_STRIP_TRAILING_WHITESPACE
+                ERROR_QUIET)
+        else()
+            set(EMEX64_VERSION_TAG "untagged")
+            execute_process(
+                COMMAND ${GIT_EXECUTABLE} rev-list --count HEAD
+                OUTPUT_VARIABLE EMEX64_COMMITS_SINCE_TAG
+                OUTPUT_STRIP_TRAILING_WHITESPACE
+                ERROR_QUIET)
+        endif()
+    endif()
 endif()
 
-if(NOT EMEX64_BUILD_NUMBER)
-    set(EMEX64_BUILD_NUMBER "0")
-endif()
 if(NOT EMEX64_GIT_HASH)
     set(EMEX64_GIT_HASH "unknown")
 endif()
+
 if(NOT EMEX64_VERSION_TAG)
     set(EMEX64_VERSION_TAG "untagged")
 endif()
 
-set(EMEX64_VERSION_FULL "EmexToolchain-${EMEX64_VERSION}-${EMEX64_VERSION_TAG}+${EMEX64_BUILD_NUMBER}.g${EMEX64_GIT_HASH}")
+if(EMEX64_COMMITS_SINCE_TAG EQUAL 0)
+    set(EMEX64_VERSION_FULL
+        "EmexToolchain-${EMEX64_VERSION}-${EMEX64_VERSION_TAG}")
+else()
+    set(EMEX64_VERSION_FULL
+        "EmexToolchain-${EMEX64_VERSION}-${EMEX64_VERSION_TAG}+${EMEX64_COMMITS_SINCE_TAG}.g${EMEX64_GIT_HASH}")
+endif()
 
 configure_file(${SRC} ${DST} @ONLY)
